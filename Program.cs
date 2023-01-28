4fcbc2fe3c8686c5121ea9ff0cb278b75be5994d
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-01-28 17:14:05 +0530

    day3

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Swap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            Console.WriteLine("Enter  first number");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second Number");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before Swapp ");
+            c = a;
+            a = b;
+            b = c;
+            Console.WriteLine("Wapping of number:");
+
+        }
+    }
+}
